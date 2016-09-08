592b30140ff71d819c551848f081a0cd932269a7
ANIL <ABCD@ABCD.com>
2016-09-08 16:13:46 +0530

    test

--- src/main/java/ma/example/domain/task/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package ma.example.domain.task;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TaskRepository extends MongoRepository<Task, String>{
-	
-	Task findByTaskId(String taskId);
-	
-}
