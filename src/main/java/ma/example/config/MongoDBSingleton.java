bdcca991a741bff05e0ff03e0f094d2a8965d890
ANIL <ABCD@ABCD.com>
2016-09-14 22:05:34 +0530

    test

--- src/main/java/ma/example/config/MongoDBSingleton.java
+++ src/main/java/ma/example/config/MongoDBSingleton.java
@@ -8,57 +8,42 @@ import com.mongodb.MongoClientURI;
 
 public class MongoDBSingleton {
 
- private static MongoDBSingleton mDbSingleton;
- 
- private static MongoClient mongoClient;
-    
- private static DB db ;
- 
- 
- private static final String dbHost = "ds019846.mlab.com";
- private static final int dbPort = 19846;
- private static final String dbName = "springtest";
- private static final String dbUser = "anil1883";
- private static final String dbPassword = "anil1883";
+	private static MongoDBSingleton mDbSingleton;
 
- private MongoDBSingleton(){};
- 
- public static MongoDBSingleton getInstance(){
-  if(mDbSingleton == null){
-   mDbSingleton = new MongoDBSingleton();
-  }
-  return mDbSingleton;
- } 
- 
- public DB getTestdb() {
-	 
-	 
-	 
-	//link is: mongodb://<user>:<password>@ds<port>.mlab.com:<port>/<db>
-	    MongoClientURI uri  = new MongoClientURI("mongodb://anil1883:anil1883@ds019846.mlab.com:19846/springtest");
-	    MongoClient client;
+	private static MongoClient mongoClient;
+
+	private static DB db;
+
+	private static final String dbHost = "ds019846.mlab.com";
+	private static final int dbPort = 19846;
+	private static final String dbName = "springtest";
+	private static final String dbUser = "anil1883";
+	private static final String dbPassword = "anil1883";
+
+	private MongoDBSingleton() {
+	};
+
+	public static MongoDBSingleton getInstance() {
+		if (mDbSingleton == null) {
+			mDbSingleton = new MongoDBSingleton();
+		}
+		return mDbSingleton;
+	}
+
+	public DB getTestdb() {
+
+		// link is: mongodb://<user>:<password>@ds<port>.mlab.com:<port>/<db>
+		MongoClientURI uri = new MongoClientURI("mongodb://anil1883:anil1883@ds019846.mlab.com:19846/springtest");
+		MongoClient client;
 		try {
-			client = new MongoClient(uri);
-		
-	    System.out.println(client.getDB("springtest")+"=======================================");
+			mongoClient = new MongoClient(uri);
+			db = mongoClient.getDB("springtest");
+
+			System.out.println(db + "=======================================");
 		} catch (UnknownHostException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-	    
-	    
-  if(mongoClient == null){
-   try {
-    mongoClient = new MongoClient(dbHost , dbPort);
-   } catch (UnknownHostException e) {
-    return null;
-   }
-  }
-  if(db == null)
-   db = mongoClient.getDB(dbName);
-  if(!db.isAuthenticated()){
-   boolean auth = db.authenticate(dbUser, dbPassword.toCharArray());
-  }
-  return db;
- }
+		return db;
+	}
 }
