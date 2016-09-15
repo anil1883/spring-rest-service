033c2980d4797d5beddeafdfd93217512b332daf
ANIL <ABCD@ABCD.com>
2016-09-15 15:53:38 +0530

    test

--- src/main/java/ma/example/config/MongoDBSingleton.java
+++ src/main/java/ma/example/config/MongoDBSingleton.java
@@ -1,16 +1,11 @@
 package ma.example.config;
 
 import java.net.UnknownHostException;
-import java.util.Arrays;
-import java.util.Set;
 
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.mongodb.MongoCredential;
 import com.mongodb.MongoURI;
-import com.mongodb.ServerAddress;
-import com.mongodb.WriteConcern;
 
 public class MongoDBSingleton {
 
@@ -38,36 +33,24 @@ public class MongoDBSingleton {
 
 	public DB getTestdb() {
 
-		// link is: mongodb://<user>:<password>@ds<port>.mlab.com:<port>/<db>
-		MongoURI mongoURI = new MongoURI("mongodb://anil1883:anil1883@ds019846.mlab.com:19846/springtest");
 		// MongoURI mongoURI = new MongoURI(System.getenv("MONGOHQ_URL"));
-		// get connected
-		DB db = null;
+		MongoURI mongoURI = new MongoURI(
+				System.getenv("mongodb://anil1883:anil1883@ds019846.mlab.com:19846/springtest"));
 
-		try {
-			db = mongoURI.connectDB();
-			db.authenticate(mongoURI.getUsername(), mongoURI.getPassword());
-			Set<String> colls = db.getCollectionNames();
-			System.out.println("Collections found in DB: " + colls.toString());
-
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if (mongoClient == null) {
+			try {
+				mongoClient = new MongoClient(dbHost, dbPort);
+			} catch (UnknownHostException e) {
+				return null;
+			}
 		}
-		// authenticate
-		// (version 2.7.2) db.authenticate(mongoURI.getUsername(),
-		// mongoURI.getPassword());
-
+		if (db == null)
+			db = mongoClient.getDB(dbName);
+		if (!db.isAuthenticated()) {
+			System.out.println("+++++++++++++++++++++++++++++++++++");
+			boolean auth = db.authenticate(dbUser, dbPassword.toCharArray());
+			System.out.println("+++++++++++++++++++++++++++++++++++"+auth);
+		}
 		return db;
-
-		/*
-		 * if(mongoClient == null){ try { mongoClient = new MongoClient(dbHost ,
-		 * dbPort); } catch (UnknownHostException e) { return null; } } if(db ==
-		 * null) db = mongoClient.getDB(dbName); if(!db.isAuthenticated()){
-		 * System.out.println("+++++++++++++++++++++++++++++++++++"); boolean
-		 * auth = db.authenticate(dbUser, dbPassword.toCharArray());
-		 * System.out.println("+++++++++++++++++++++++++++++++++++"); } return
-		 * db;
-		 */
 	}
 }
