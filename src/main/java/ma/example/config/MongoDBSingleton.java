59ee6acc75164a1e52bc1d0ac7885c044b22d210
ANIL <ABCD@ABCD.com>
2016-09-16 10:07:39 +0530

    test

--- src/main/java/ma/example/config/MongoDBSingleton.java
+++ src/main/java/ma/example/config/MongoDBSingleton.java
@@ -1,6 +1,5 @@
 package ma.example.config;
 
-import java.net.UnknownHostException;
 import java.util.Arrays;
 
 import com.mongodb.DB;
@@ -13,16 +12,7 @@ import com.mongodb.WriteConcern;
 public class MongoDBSingleton {
 
 	private static MongoDBSingleton mDbSingleton;
-
-	private static MongoClient mongoClient;
-
 	private static DB db;
-
-	private static final String dbHost = "ds019846.mlab.com";
-	private static final int dbPort = 19846;
-	private static final String dbName = "springtest";
-	private static final String dbUser = "anil1883";
-	private static final String dbPassword = "anil1883";
 
 	private MongoDBSingleton() {
 	};
@@ -36,42 +26,22 @@ public class MongoDBSingleton {
 
 	public DB getTestdb() {
 
-		// MongoURI mongoURI = new MongoURI(System.getenv("MONGOHQ_URL"));
-	/*	MongoURI mongoURI = new MongoURI(
-				System.getenv("mongodb://anil1883:anil1883@ds019846.mlab.com:19846/springtest"));*/
-		MongoURI mongoURI = new MongoURI("mongodb://anil1883:anil1883@ds019846.mlab.com:19846/springtest");
-		
-	    //get connected
+		//MongoURI mongoURI = new MongoURI(System.getenv("MONGOHQ_URL"));
+		 MongoURI mongoURI = new
+		 MongoURI("mongodb://anil1883:anil1883@ds019846.mlab.com:19846/springtest");
+
+		// get connected
 		try {
 			db = mongoURI.connectDB();
-			 MongoCredential credential = MongoCredential.createMongoCRCredential(mongoURI.getUsername(), mongoURI.getDatabase(), mongoURI.getPassword());
-			 MongoClient mongoClient = new MongoClient(new ServerAddress(), Arrays.asList(credential));
-			 mongoClient.setWriteConcern(WriteConcern.JOURNALED);
-			
+			MongoCredential credential = MongoCredential.createMongoCRCredential(mongoURI.getUsername(),
+					mongoURI.getDatabase(), mongoURI.getPassword());
+			MongoClient mongoClient = new MongoClient(new ServerAddress(), Arrays.asList(credential));
+			mongoClient.setWriteConcern(WriteConcern.JOURNALED);
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		  
-		    
-	    // authenticate
-	    // (version 2.7.2) db.authenticate(mongoURI.getUsername(), mongoURI.getPassword());
-	   
-
-		/*if (mongoClient == null) {
-			try {
-				mongoClient = new MongoClient(dbHost, dbPort);
-			} catch (UnknownHostException e) {
-				return null;
-			}
-		}
-		if (db == null)
-			db = mongoClient.getDB(dbName);
-		if (!db.isAuthenticated()) {
-			System.out.println("+++++++++++++++++++++++++++++++++++");
-			boolean auth = db.authenticate(dbUser, dbPassword.toCharArray());
-			System.out.println("+++++++++++++++++++++++++++++++++++"+auth);
-		}*/
 		return db;
 	}
 }
--- src/main/java/org/springframework/samples/mvc/simple/WebController.java
+++ src/main/java/org/springframework/samples/mvc/simple/WebController.java
@@ -1,16 +1,18 @@
 package org.springframework.samples.mvc.simple;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
@@ -20,106 +22,79 @@ import com.mongodb.DBObject;
 
 import ma.example.config.Books;
 import ma.example.config.MongoDBSingleton;
-  
-    
-@Path("/webservice")  
-@Controller
-public class WebController { 
-	
+
+@RestController
+public class WebController {
+
 	@RequestMapping("/helloTest")
 	public @ResponseBody String simple() {
-		 MongoDBSingleton dbSingleton = MongoDBSingleton.getInstance();
-    	 DB db = dbSingleton.getTestdb();
-    	 System.out.println(db+"=============================");
-    	 DBCollection coll = db.getCollection("Books");	
-    	 
-    	 BasicDBObject doc = new BasicDBObject("title", "anmol").
-    			 append("description", "my name is anmol").
-    			 append("likes", "2").
-    			 append("year", "2016").
-    			 append("by", "anil");
-    	 coll.insert(doc);
-    	 DBCursor cursor = coll.find().sort(new BasicDBObject("by", 1));
-    	 List<Books> list = new ArrayList<Books>();
-    	 while (cursor.hasNext()) { 
-             DBObject o = cursor.next();
-             Books bools = new Books();
-             bools.setTitle((String) o.get("title"));
-             bools.setDescription((String) o.get("description"));
-             bools.setYear((String) o.get("year"));
-             bools.setBy((String) o.get("by"));
-             bools.setLikes((String) o.get("likes"));
-             list.add(bools);
-          }
-    	 System.out.println(list.size()+"=============================");
+		MongoDBSingleton dbSingleton = MongoDBSingleton.getInstance();
+		DB db = dbSingleton.getTestdb();
+		System.out.println(db + "=============================");
+		DBCollection coll = db.getCollection("Books");
+		System.out.println(coll + "=============================");
 		return "Hello world!";
 	}
-   
-     @GET  
-     @Path("/echo/{message}")  
-     @Produces("text/plain")  
-     public String showMsg(@PathParam("message") String message){  
-         return message;      
-     }  
-       
-     
-    /* @GET 
-     @Path("/insert/{name}/{by}/{likes}/{year}/{description}")  
-     @Produces("text/plain")  
-     public String insert(@PathParam("name") String name  , @PathParam("description") String description , @PathParam("likes") Long likes , @PathParam("year") String year , @PathParam("by") String by){
-    	 MongoDBSingleton dbSingleton = MongoDBSingleton.getInstance();
-    	 DB db = dbSingleton.getTestdb();
-    	 DBCollection coll = db.getCollection("Books");	
-    	 BasicDBObject doc = new BasicDBObject("title", name).
-    			 append("description", description).
-    			 append("likes", likes).
-    			 append("year", year).
-    			 append("by", by);
-    	 coll.insert(doc);
-    	 return db.isAuthenticated() + " ; " + db.getName();
 
-     }  */
-     
-     @GET 
-     @Path("/getRecords")  
-     @Produces(MediaType.APPLICATION_JSON)  
-     public List<Books> getRecords(){
-    	 MongoDBSingleton dbSingleton = MongoDBSingleton.getInstance();
-    	 DB db = dbSingleton.getTestdb();
-    	 DBCollection coll = db.getCollection("Books");	
-    	 DBCursor cursor = coll.find().sort(new BasicDBObject("by", 1));
-    	 List<Books> list = new ArrayList<Books>();
-    	 while (cursor.hasNext()) { 
-             DBObject o = cursor.next();
-             Books bools = new Books();
-             bools.setTitle((String) o.get("title"));
-             bools.setDescription((String) o.get("description"));
-             bools.setYear((String) o.get("year"));
-             bools.setBy((String) o.get("by"));
-           //  bools.setLikes((Long) o.get("likes"));
-             list.add(bools);
-          }
-    	 return list;
-     } 
-     
-     @GET 
-     @Path("/getRecord/{title}")  
-     @Produces(MediaType.APPLICATION_JSON)  
-     public List<Books> getRecordFromName(@PathParam("title") String message){
-    	 MongoDBSingleton dbSingleton = MongoDBSingleton.getInstance();
-    	 DB db = dbSingleton.getTestdb();
-    	 DBCollection coll = db.getCollection("Books");	
-    	 DBCursor cursor = coll.find(new BasicDBObject("title", message));
-    	 List<Books> list = new ArrayList<Books>();
-    	 while (cursor.hasNext()) { 
-             DBObject o = cursor.next();
-             Books bools = new Books();
-             bools.setTitle((String) o.get("title"));
-             bools.setDescription((String) o.get("description"));
-             bools.setYear((String) o.get("year")); 
-             bools.setBy((String) o.get("by"));
-             list.add(bools);
-          }
-    	 return list;
-     } 
-}  
+	@GET
+	@RequestMapping("/echo/{message}")
+	@Produces("text/plain")
+	public String showMsg(@PathParam("message") String message) {
+		return message;
+	}
+
+	@GET
+	@RequestMapping(value = "/insert/{name}/{by}/{likes}/{year}/{description}", method = RequestMethod.POST, produces = MediaType.TEXT_PLAIN)
+	@Produces("text/plain")
+	public String insert(@PathParam("name") String name, @PathParam("description") String description,
+			@PathParam("likes") Long likes, @PathParam("year") String year, @PathParam("by") String by) {
+		MongoDBSingleton dbSingleton = MongoDBSingleton.getInstance();
+		DB db = dbSingleton.getTestdb();
+		DBCollection coll = db.getCollection("Books");
+		System.out.println(name);
+		BasicDBObject doc = new BasicDBObject("title", name).append("description", description).append("likes", likes)
+				.append("year", year).append("by", by);
+		coll.insert(doc);
+		return "Done";
+
+	}
+
+	@RequestMapping("/getRecords")
+	public List<Books> getRecords() {
+		MongoDBSingleton dbSingleton = MongoDBSingleton.getInstance();
+		DB db = dbSingleton.getTestdb();
+		DBCollection coll = db.getCollection("Books");
+		DBCursor cursor = coll.find().sort(new BasicDBObject("by", 1));
+		List<Books> list = new ArrayList<Books>();
+		while (cursor.hasNext()) {
+			DBObject o = cursor.next();
+			Books bools = new Books();
+			bools.setTitle((String) o.get("title"));
+			bools.setDescription((String) o.get("description"));
+			bools.setYear((String) o.get("year"));
+			bools.setBy((String) o.get("by"));
+			// bools.setLikes((Long) o.get("likes"));
+			list.add(bools);
+		}
+		return list;
+	}
+
+	@RequestMapping(value = "/getRecord/{title}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
+	public List<Books> getRecordFromName(@PathVariable("title") String message) {
+		MongoDBSingleton dbSingleton = MongoDBSingleton.getInstance();
+		DB db = dbSingleton.getTestdb();
+		DBCollection coll = db.getCollection("Books");
+		DBCursor cursor = coll.find(new BasicDBObject("title", message));
+		List<Books> list = new ArrayList<Books>();
+		while (cursor.hasNext()) {
+			DBObject o = cursor.next();
+			Books bools = new Books();
+			bools.setTitle((String) o.get("title"));
+			bools.setDescription((String) o.get("description"));
+			bools.setYear((String) o.get("year"));
+			bools.setBy((String) o.get("by"));
+			list.add(bools);
+		}
+		return list;
+	}
+}
