df8a1495930cb876cf937460a75450f3568f4b14
ANIL <ABCD@ABCD.com>
2016-09-16 17:37:16 +0530

    test

--- src/main/java/com/jcg/examples/dao/impl/UserDaoImpl.java
+++ src/main/java/com/jcg/examples/dao/impl/UserDaoImpl.java
@@ -19,21 +19,20 @@ import ma.example.config.MongoDBSingleton;
  * @author CENTAUR
  */
 public class UserDaoImpl implements UserDao {
-	
+
 	@Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@Override
 	public boolean isValidUser(String username, String password) throws SQLException {
 		MongoDBSingleton dbSingleton = MongoDBSingleton.getInstance();
-		System.out.println(username+password);
+		System.out.println(username + password);
 		DB db = dbSingleton.getTestdb();
 		DBCollection coll = db.getCollection("Users");
 		DBCursor cursor = coll.find(new BasicDBObject("username", username));
 		while (cursor.hasNext()) {
 			DBObject o = cursor.next();
-			String temp = bCryptPasswordEncoder.encode((CharSequence) o.get("password"));
-			if (password.equals(temp))
+			if (bCryptPasswordEncoder.matches(password, (String) o.get("password")))
 				return true;
 		}
 
@@ -45,11 +44,12 @@ public class UserDaoImpl implements UserDao {
 		MongoDBSingleton dbSingleton = MongoDBSingleton.getInstance();
 		DB db = dbSingleton.getTestdb();
 		DBCollection coll = db.getCollection("Users");
-		
+
 		String temp = bCryptPasswordEncoder.encode(user.getPassword());
-		
-		BasicDBObject doc = new BasicDBObject("username", user.getUsername()).append("password", temp).append("email", user.getEmail())
-				.append("birth date", user.getBirthDate()).append("profession", user.getProfession());
+
+		BasicDBObject doc = new BasicDBObject("username", user.getUsername()).append("password", temp)
+				.append("email", user.getEmail()).append("birth date", user.getBirthDate())
+				.append("profession", user.getProfession());
 		coll.insert(doc);
 		return false;
 	}
